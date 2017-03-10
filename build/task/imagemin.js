051b8e55486183f9d9b8f802a9c3d08279987aa6
侯正华 <houzhenghua.sh@superjia.com>
2017-03-10 19:08:00 +0800

    add

--- build/task/imagemin.js
+++ build/task/imagemin.js
@@ -7,19 +7,19 @@ gulp.task('imagemini', function (done) {
     console.time('time');
     let t1 = +new Date;
     // gulp.src('src/**/*.min.png')
-     gulp.src('src/**/*.min.+(png|jpg|jpeg|gif)')
+    gulp.src('src/**/*.min.+(png|jpg|jpeg|gif)')
         .pipe(tinypng('app_key(去tinypng网站申请)'))
         .pipe(gulp.dest('src'))
-        .on('end',function(){
+        .on('end', function () {
             let t2 = +new Date;
-            console.error(`图片已经压缩完毕====================合计用时:${Math.round((t2-t1)/1000)}s`);
+            console.error(`图片已经压缩完毕!\n合计用时:${Math.round((t2 - t1) / 1000)}s`);
             done();// callback to signal end of build
         });
 });
 
-gulp.task('default',function(){
-    if (gulp.tasks.imagemini) { 
-         console.log('图片正则压缩中======================');
-         gulp.start('imagemini');
+gulp.task('default', function () {
+    if (gulp.tasks.imagemini) {
+        console.log('图片正在压缩中..................');
+        gulp.start('imagemini');
     }
 });

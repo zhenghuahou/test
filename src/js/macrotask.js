50f7472389fe11d56d357741f01ac96811f1f121
侯正华 <houzhenghua@qingting.fm>
2020-03-12 22:15:42 +0800

    add

--- src/js/macrotask.js
+++ src/js/macrotask.js
@@ -76,3 +76,30 @@ console.log('script end')
 // setTimeout(_ => console.log('timeout')) // 注册宏任务
 // console.log('click end') // 直接输出
 // click start --> click end --> promise -->timeout
+
+
+
+
+
+console.log('Message no. 1: Sync',+new Date);
+setTimeout(function() {
+   console.log('Message no. 2: setTimeout',+new Date);
+}, 0);
+var promise = new Promise(function(resolve, reject) {
+    console.warn('同步代码',+new Date)
+   resolve();
+});
+promise.then(function(resolve) {
+   console.log('Message no. 3: 1st Promise',+new Date);
+})
+.then(function(resolve) {
+   console.log('Message no. 4: 2nd Promise',+new Date);
+});
+console.log('Message no. 5: Sync',+new Date);
+
+//Message no. 1: Sync
+//同步代码
+//Message no. 5: Sync
+//Message no. 3: 1st Promise'
+//Message no. 4: 2nd Promise
+//Message no. 2: setTimeout

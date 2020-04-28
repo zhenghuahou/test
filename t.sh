cba3661e5f49495ac64e97c4c340d17ab45ea8c4
houzhenghua <houzhenghua@zhongan.com>
2020-04-28 11:41:46 +0800

    自定义git脚本

---
+++ t.sh
@@ -0,0 +1,37 @@
+
+#!/bin/bash
+
+
+
+# result=$(git branch --contains $1)
+# echo "Commit SHA1: $1"
+# echo "Branch name: $result"
+
+
+# result2=$(git diff --stat HEAD^..HEAD)
+# echo "$result2"
+
+# result3=$(git cherry-pick -n $1)
+# echo "$result3"
+
+
+#!/bin/bash
+hasGit=`which git` # 判断是否存在git
+msg=${1:-'auto commit'} # 获取终端输入的第一个参数，若为空则为auto commit
+echo git:"$hasGit"
+echo msg:"$msg"
+
+
+if [ ! $hasGit ];then
+  echo 'Please download git first!';
+  exit 1;
+else 
+  result=`git branch | grep "*"` # 获取分支名
+  curBranch=${result:2} # 去除多余的*
+  echo result:"$result"
+  echo 当前分支:"$curBranch"
+  git add .
+  git commit -m "$msg"
+  git push
+fi
+
